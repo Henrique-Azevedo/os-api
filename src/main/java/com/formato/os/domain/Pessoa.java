5f881c002139d84ebd023417f8742b648e19b26d
Henrique-Azevedo <henrique.azevedo.pro@gmail.com>
2022-10-27 22:20:37 -0300

    initial commit

---
+++ src/main/java/com/formato/os/domain/Pessoa.java
@@ -0,0 +1,19 @@
+package com.formato.os.domain;
+
+public abstract class Pessoa {
+	
+	private Integer id;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	
+	public Pessoa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+
+}
